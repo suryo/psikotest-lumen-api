fe404a7cc5d347314da94d0713e15614789464f0
suryo <suryoatm@gmail.com>
2022-06-07 16:27:02 +0700

    alter Vis_user models

--- app/Models/vis_user.php
+++ app/Models/vis_user.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class vis_user extends Model
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Vis_user extends Model
 {
     use HasFactory;
 
@@ -14,3 +19,28 @@ class vis_user extends Model
     ];
 
 }
+
+
+// class Vis_user extends Model implements AuthenticatableContract, AuthorizableContract
+// {
+//     use Authenticatable, Authorizable, HasFactory;
+
+//     /**
+//      * The attributes that are mass assignable.
+//      *
+//      * @var array
+//      */
+//     protected $fillable = [
+//         'name', 'email',
+//     ];
+
+//     /**
+//      * The attributes excluded from the model's JSON form.
+//      *
+//      * @var array
+//      */
+//     protected $hidden = [
+//         'password',
+//     ];
+// }
+
